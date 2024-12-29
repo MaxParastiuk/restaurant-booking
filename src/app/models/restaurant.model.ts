ed4a42815b0e781b7d5455b2c809af6860dc45cb
MaxParastiuk <Parastukm@gmail.com>
2024-12-29 12:49:19 +0100

    comments adjustment

--- src/app/models/restaurant.model.ts
+++ src/app/models/restaurant.model.ts
@@ -2,24 +2,24 @@ export interface Restaurant {
   id: number;
   name: string;
   address: string;
-  cuisineType: string; // Тип кухни
-  priceRange: string; // Ценовой диапазон, например, "$", "$$", "$$$"
-  averageCheck: number; // Средний чек
-  description: string; // Описание ресторана
-  workingHours: WorkingHours; // Время работы (подтип)
-  features: string[]; // Особенности, например, ["Wi-Fi", "Outdoor Seating"]
-  reviews: Review[]; // Отзывы (подтип)
+  cuisineType: string;
+  priceRange: string;
+  averageCheck: number;
+  description: string;
+  workingHours: WorkingHours;
+  features: string[];
+  reviews: Review[];
 }
 
 interface WorkingHours {
-  open: string; // Время открытия, например, "08:00 AM"
-  close: string; // Время закрытия, например, "10:00 PM"
-  days: string[]; // Дни работы, например, ["Monday", "Tuesday", "Wednesday"]
+  open: string;
+  close: string;
+  days: string[];
 }
 
 interface Review {
-  reviewerName: string; // Имя рецензента
-  rating: number; // Оценка, например, 4.5
-  comment: string; // Текст отзыва
-  date: string; // Дата отзыва
+  reviewerName: string;
+  rating: number;
+  comment: string;
+  date: string;
 }
